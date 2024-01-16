4d853a951ab04f2a2c53fe9b191929b76548bf12
Austin Davis <AustinDavis@gmail.com>
2024-01-16 17:37:13 -0500

    testing ddp and lr changes

--- config/train_lichess_uci.py
+++ config/train_lichess_uci.py
@@ -1,19 +1,19 @@
 # train a miniature character-level shakespeare model
 
 out_dir = 'out-lichess-uci'
-eval_interval = 2000 # keep frequent because we'll overfit
-eval_iters = 100
-log_interval = 100 # don't print too too often
+eval_interval = 1000 # keep frequent because we'll overfit
+eval_iters = 100 # number of batch iterations over which we eval
+log_interval = 50 # don't print too too often
 init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 
-always_save_checkpoint = True
+always_save_checkpoint = False
 
 wandb_log = True # override via command line if you like
 wandb_project = 'chess-gpt-batch'
-wandb_run_name = 'lichess_uci_all_elos_8layers'
+wandb_run_name = 'lichess_uci_all_elos_8layers_new_lr'
 
 dataset = 'lichess_uci_hf_dataset'
-gradient_accumulation_steps = 8 # this must be evently divisiable by ddp world size
+gradient_accumulation_steps = 4 # this must be evently divisiable by ddp world size
 batch_size = 400
 block_size = 1023 # context of up to block_size previous characters
 
@@ -23,15 +23,11 @@ n_head = 8
 n_embd = 512
 dropout = 0.0
 
-learning_rate = 3e-4 # with baby networks can afford to go a bit higher
-max_iters = 6e5
-lr_decay_iters = 6e5 # make equal to max_iters usually
-min_lr = 3e-5 # learning_rate / 10 usually
+learning_rate = 3e-4 * batch_size / 50. # with baby networks can afford to go a bit higher
+max_iters = 150_000
+lr_decay_iters = 150_000 # make equal to max_iters usually
+min_lr = learning_rate / 10  # learning_rate / 10 usually (orig: 3e-5)
 beta2 = 0.95 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 10000 # not super necessary potentially
+warmup_iters = 0 # not super necessary potentially
 compile = True
-
-# on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
